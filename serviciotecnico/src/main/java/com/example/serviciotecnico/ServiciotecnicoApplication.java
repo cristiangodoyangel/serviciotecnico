11a011702971ddf4b2be14b85f369011332aad12
cristiangodoyangel <creacciones@gmail.com>
2025-12-03 10:12:36 -0300

    isntall: se instala Angular

---
+++ serviciotecnico/src/main/java/com/example/serviciotecnico/ServiciotecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviciotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciotecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciotecnicoApplication.class, args);
+	}
+
+}
---
+++ serviciotecnico/src/test/java/com/example/serviciotecnico/ServiciotecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviciotecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciotecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
